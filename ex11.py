566fcfb27a7d92a50275fb714b98a47c3e2a70b4
chlin <chlin.liu@gmail.com>
2014-10-17 15:41:57 +0800

    add all examples

---
+++ ex11.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+print("How old are you?")
+age = int(input())
+
+print("How tall are you?")
+height = int(input())
+print("How much do you weight?")
+weight = int(input())
+
+print("So, you're {} old, {} tall and {} heavy.".format(age, height, weight))
